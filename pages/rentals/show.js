3df3e42c74c872376b4345e3346ae47480569da6
Hendrik Bilges <elektronaut0815@users.noreply.github.com>
2018-08-22 18:14:18 +0200

    initial version of rental offer details page with routing

---
+++ pages/rentals/show.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class RentalOfferShow extends Component {
+  render() {
+    return <h3>Rental Offer Show</h3>;
+  }
+}
+
+export default RentalOfferShow;
--- routes.js
+++ routes.js
@@ -1,3 +1,7 @@
 const routes = require('next-routes')();
 
+routes
+  .add('/rentals/new', '/rentals/new')
+  .add('/rentals/:address', '/rentals/show');
+
 module.exports = routes;
